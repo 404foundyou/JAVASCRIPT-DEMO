dee5565163b787dac6078003db44d5f593fe842a
404foundyou <attendrabhushan@gmail.com>
2026-01-06 19:43:42 +0530

    Two examples Solved

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-console.log("hellow from script!");
-console.log(document);
+// dom manipulation consist of 
+// 1.get the element
+// 2.event listening
+
+
+//example 1
+document
+.getElementById("changetextbutton")
+.addEventListener('click', function (){
+    let paragraph = document.getElementById("myParagraph");
+    paragraph.textContent = "The Paragraph is changed";
+});
+
+// .addEventListener('click',  () =>{ This will give you the window obj
+//     console.log(this)
+// }) 
+
+
+//example-2
+document
+.getElementById("highlightFirstCity")
+.addEventListener('click' , function(){
+    let citylist = document.getElementById("citiesList");
+    console.log(citylist.firstElementChild);
+});
+
